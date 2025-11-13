687a2ea232ace076ceb261e3c08e4dcaa863ebdf
rakib010y <rakib010y@gmail.com>
2025-11-13 11:33:56 +0600

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -1,16 +1,9 @@
-'use client';
-
-import { WalletProvider } from '@suiet/wallet-kit';
-import '@suiet/wallet-kit/style.css';
-
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <head />
       <body>
-        <WalletProvider>
-          {children}
-        </WalletProvider>
+        {children}
       </body>
     </html>
   );
